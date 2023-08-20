7f89c06c7067b6c5fb147de7a759a6a8bf51c8f4
programminghero <softhandshakil@gmail.com>
2023-08-20 06:10:58 +0600

    Alhamdulillah discount part done

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,8 @@
 function productValue(price) {
-    console.log(price);
+    // console.log(price);
 
     // call the total price without discount
-  
+
     totalPriceMain(price);
 
 
@@ -10,7 +10,7 @@ function productValue(price) {
 }
 
 
-function totalPriceMain(price){
+function totalPriceMain(price) {
     const totalPrice = document.getElementById('totalPrice');
     const totalPriceString = totalPrice.innerText;
     const totalPriceValue = parseFloat(totalPriceString);
@@ -20,6 +20,40 @@ function totalPriceMain(price){
     // console.log(finalTotal);
 
     totalPrice.innerText = finalTotal;
+
+    if (finalTotal > 0) {
+        console.log('Enabled');
+        document.getElementById("purchaseButton").disabled = false;
+    }
+
+    if (finalTotal >= 200) {
+
+        document.getElementById("cuponButton").disabled = false;
+
+        // take the value from cupon
+        document.getElementById('cuponButton').addEventListener('click',function(){
+            const cuponInput = document.getElementById('cupon');
+            const cuponValue = cuponInput.value;
+            // console.log(cuponValue);
+
+            if(cuponValue == 'SELL200'){
+                const discount = 20 * finalTotal / 100;
+                console.log('discount', discount);
+
+                // call the discount default amount
+                const discountAmount = document.getElementById('discountAmount');
+                
+                
+                // set the discount value
+                discountAmount.innerText = discount; 
+            }
+            else{
+                console.log('invalid cupon');
+            }
+        })
+        
+
+    }
 
 
 }
